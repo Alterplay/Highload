7975fe9a6ca48e0c91a1df92d77109e18e427fb4
EvgeniyGT <evgeniy.gt@gmail.com>
2014-12-29 19:12:24 +0200

    Ready for review

--- Highload/ViewControllers/HardDiskViewController.swift
+++ Highload/ViewControllers/HardDiskViewController.swift
@@ -8,14 +8,11 @@
 
 import UIKit
 
-class HardDiskViewController: BaseViewController {
+class HardDiskViewController: BaseViewController, UIActionSheetDelegate {
  
     //Strings values
     let totalSpaceText : String = "Total space:"
     let avaliableSpaceText : String = "Avaliable space:"
-    
-    //The number formatter
-    lazy var numberFormatter = NSByteCountFormatter()
     
     //Current mocker
     lazy var hardDiskMocker = HardDiskMocker(delegateQueue:dispatch_get_main_queue())
@@ -34,9 +31,14 @@ class HardDiskViewController: BaseViewController {
     //MARK: Actions
     
     @IBAction func runButtonClicked(sender: AnyObject) {
-        self.hardDiskMocker.fillTheDisc { () -> Void in
-            self.updateUI()
-        }
+        let actionSheet : UIActionSheet = UIActionSheet();
+        actionSheet.addButtonWithTitle("Full disk size")
+        actionSheet.addButtonWithTitle("Leave 5 GB")
+        actionSheet.addButtonWithTitle("Leave 10 GB")
+        actionSheet.addButtonWithTitle("Cancel")
+        actionSheet.cancelButtonIndex = 3;
+        actionSheet.delegate = self
+        actionSheet.showInView(self.view)
     }
     
     //MARK: Private
@@ -51,11 +53,42 @@ class HardDiskViewController: BaseViewController {
         let totalSize : NSNumber = info[NSFileSystemSize] as NSNumber
         let avaliable : NSNumber = info[NSFileSystemFreeSize] as NSNumber
         
-        let totalSizeString = NSString(format:"%@ %@",totalSpaceText,self.numberFormatter.stringFromByteCount(Int64(totalSize.doubleValue)))
-        let avaliableSizeString = NSString(format:"%@ %@",avaliableSpaceText,self.numberFormatter.stringFromByteCount(Int64(avaliable.doubleValue)))
-        
-        self.totalSpaceLabel.text = totalSizeString
-        self.avaliableSpaceLabel.text = avaliableSizeString
+        self.totalSpaceLabel.text = NSString(format:"%@ %@", totalSpaceText, self.convertValue(totalSize))
+        self.avaliableSpaceLabel.text = NSString(format:"%@ %@", avaliableSpaceText, self.convertValue(avaliable))
     }
-
+    
+    /**
+    Size converter
+    */
+    func convertValue(sizeValue: NSNumber) -> String {
+        var convertedValue = sizeValue.doubleValue
+        var multiplyFactor : Int = 0
+        let tokens : NSArray = ["bytes", "KB", "MB", "GB", "TB"]
+        
+        while (convertedValue > 1024) {
+            convertedValue /= 1024
+            multiplyFactor++
+        }
+        let resultToken : NSString = tokens.objectAtIndex(multiplyFactor) as NSString
+        return NSString(format:"%4.2f %@", convertedValue, resultToken)
+    }
+    
+    
+    /**
+    UIActionSheetDelegate
+    */
+    func actionSheet(myActionSheet: UIActionSheet!, clickedButtonAtIndex buttonIndex: Int) {
+        
+        var updateBlock = { () -> Void in
+            self.updateUI()
+        }
+        
+        switch buttonIndex
+        {
+        case 0 : self.hardDiskMocker.fillTheDiscWithLimit(0, tick: updateBlock) /* Full */
+        case 1 : self.hardDiskMocker.fillTheDiscWithLimit(5368709120, tick: updateBlock) /* Leave 5GB */
+        case 2 : self.hardDiskMocker.fillTheDiscWithLimit(10737418240, tick: updateBlock) /* Leave 10GB */
+        default: println("cancel..")
+        }
+    }
 }
